bb9b4eca661e7322f9668bf00f655639f76b52ec
Ewerton Cardoso <ewerton.code@gmail.com>
2017-08-11 11:25:33 -0300

    #22 Cria enum com status dos itens

---
+++ Projeto-Codigo/SgplCamadaApicacao/src/br/edu/ifnmg/psc/sgpl/aplicacao/StatusItemPregao.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.psc.sgpl.aplicacao;
+
+/**
+ *
+ * @author ewertoncardoso
+ */
+public enum StatusItemPregao {
+    
+    LICITANDO(1,"Licitando item"),
+    LICITADO(2, "Produto licitado"),
+    EMPENHADO(3, "Emitido nota de empenho"),
+    ENTREGUE(4, "Produto entregue"),
+    ATRASADO(5, "Atrasado");
+    
+    private int id;
+    private String descricao;
+    
+    private StatusItemPregao(int id, String desc){
+        this.id = id;
+        this.descricao = desc;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return descricao;
+    }
+    
+    public static StatusPregao Abrir(int id){
+        for(StatusPregao t : StatusPregao.values())
+            if(id == t.getId())
+                return t;
+        return null;
+    }
+}
